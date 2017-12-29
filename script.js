a448ea9f8a6228dbcb0f95b977a46077736ea5fa
ericsauber <eric.sauber@mavs.uta.edu>
2017-12-28 23:26:08 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var forismaticAPI = 'https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?';
+
+$(document).ready(function() {
+    
+      $('#newQuote').on("click", function() {
+          $.getJSON(forismaticAPI, function(data) {
+
+              if (data.quoteAuthor === '')
+                data.quoteAuthor = 'Unknown';
+
+              $("#quotearea").hide(0).fadeIn(600);
+              $("#author").hide(0).fadeIn(600);
+              $('#quotearea').empty();           
+              $('#quotearea').append('\"' + data.quoteText + '\"');
+              $('#author').empty();           
+              $('#author').append(' - ' + data.quoteAuthor);
+          });
+      });  
+});
